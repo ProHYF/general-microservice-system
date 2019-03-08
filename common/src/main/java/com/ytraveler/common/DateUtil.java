a0e884a13e76259aac826f56346e3f28a4af8dfe
haoyufeng <haoyufeng@dafy.com>
2019-03-08 11:33:03 +0800

    初始化项目配置

---
+++ common/src/main/java/com/ytraveler/common/DateUtil.java
@@ -0,0 +1,10 @@
+package com.ytraveler.common;
+
+/**
+ *
+ * @author xss
+ * @version 1.0
+ * @date 2019-03-08 11:32
+ */
+public class DateUtil {
+}
---
+++ ucc-server-api/src/main/java/com/ytraveler/UccServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.ytraveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UccServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UccServerApiApplication.class, args);
+	}
+
+}
